54e575df93be31d5235e312628cd03f6c60c068d
hojihyun <chwu9519@uni.sydney.edu.au>
2024-09-01 17:55:39 +1000

    remove comments

--- capstone-project/src/scripts/app.tsx
+++ capstone-project/src/scripts/app.tsx
@@ -1,11 +1,4 @@
-// import { createRoot } from 'react-dom/client';
-
-// const root = createRoot(document.body);
-// root.render(<h2>Hello from React!</h2>);
-
 import { createRoot } from "react-dom/client";
-// import { Layout } from "./components/Layout";
-// import { Home } from "./components/views/Home";
 
 const container = document.getElementById('root');
 
