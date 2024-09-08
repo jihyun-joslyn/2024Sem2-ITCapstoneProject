b91c2bb33311e012caf62ef2d714709b8746e179
hojihyun <chwu9519@uni.sydney.edu.au>
2024-09-08 19:02:03 +1000

    refactor

--- capstone-project/src/scripts/app.tsx
+++ capstone-project/src/scripts/app.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Box, Grid2 as Grid } from '@mui/material';
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
-import DetailPane from '../components/DetailPane';
 import Main from '../components/Main';
-import { useState } from 'react';
 
 const container = document.getElementById('root');
 
